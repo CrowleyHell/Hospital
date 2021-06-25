267eacc00460b5c972c19f30fe1317c9d3a06bb3
CrowleyHell <n.evvio@bk.ru>
2021-06-26 02:53:50 +0300

    Pretty final

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from Auth import Authorization
-from Reg import Registration
+from Authorization import Authorization
+from Registration import Registration
 
 
 if __name__ == '__main__':
